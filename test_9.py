4f2f799f17feed584da4df8ab1fe1612b096a3da
Ievgenpolozsc <ipoloz@snapchat.com>
2018-01-29 23:52:04 +0200

    Test 9

--- test_9.py
+++ test_9.py
@@ -8,10 +8,7 @@ for i in range(0,10):
 rating_var_max = max(list)
 rating_var_min = min(list)
 
-if rating_var_max > abs(rating_var_min):
-    rating_var = rating_var_max
-else:
-    rating_var = abs(rating_var_min)
+rating_var = abs(max(rating_var_min, rating_var_max, key=abs))
 
 print('Задан список: \n\t', list)
 
@@ -19,6 +16,6 @@ for i, list_var in enumerate(list):
     if abs(list[i]) == rating_var or list[i] == 0:
         list[i] = int(list_var / rating_var)
     else:
-        list[i] = float("{0:.3f}".format(list_var / rating_var))
+        list[i] = round(list_var / rating_var, 3)
 
 print('\nНормированный список по числу %d: \n\t' % rating_var, list)
