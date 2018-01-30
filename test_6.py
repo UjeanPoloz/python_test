3e5e7fd68eabc557d007ecd7c4116ba5b9fd33f6
Ievgenpolozsc <ipoloz@snapchat.com>
2018-01-30 15:23:45 +0200

    Test 6. Move main logic to def is_isogram

--- test_6.py
+++ test_6.py
@@ -1,19 +1,20 @@
 import re
 
+def is_isogram(str_var):
+    user_str = re.sub(r'[\s]', '', var_input)
+    ind = 0
+
+    for ind, letter in enumerate(user_str):
+        for j in range(ind + 1, len(user_str)):
+
+            if letter == user_str[j]:
+                return False
+
+    return True
+
 var_input = input("Введите строку для работы с ней: ")
 
-user_str = re.sub(r'[\s]','', var_input)
-ind = 0
-duplications = False
-
-for ind, letter in enumerate(user_str):
-    for j in range(ind+1, len(user_str)):
-        if letter == user_str[j]:
-            duplications = True
-
-    if duplications:
-        print('\nСтрока не изограмма')
-        break
-
-if not duplications:
-    print('\nСтрока изограмма')
+if is_isogram(var_input):
+    print('Строка изограмма')
+else:
+    print('Строка не изограмма')
