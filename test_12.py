3c2e432785682014f76c7531d72a86ecce4216fa
Ievgenpolozsc <ipoloz@snapchat.com>
2018-01-30 11:55:18 +0200

    Test 12. In one loop

--- test_12.py
+++ test_12.py
@@ -6,33 +6,33 @@ def pretty_print_lit(lst, n):
         print('Упражнение %d: \t%s' % (i+1, lst[i]))
 
 def exercise_generator(student_count, first_number, last_number):
-    lst_exr = ['' for _ in range(student_count)]
-    lst_var = [_ for _ in range(first_number,last_number+1)]
+    lst_exr = ['']*student_count
+    lst_var = [i for i in range(first_number,last_number+1)]
 
-    for ind in range(len(lst_exr)):
-        while True:
-            var_one = random.choice(lst_var)
-            var_two = random.choice(lst_var)
-            exr = str(var_one) + ' * ' + str(var_two)
-            if is_not_duplicate(exr, lst_exr):
+    ind = 0
+
+    # for ind in range(student_count):
+    #     while True:
+    #         var_one = random.choice(lst_var)
+    #         var_two = random.choice(lst_var)
+    #         exr = str(var_one) + ' * ' + str(var_two)
+    #         if is_not_duplicate(exr, lst_exr):
+    #             lst_exr[ind] = exr
+    #             break
+
+    while ind != student_count:
+        var_one = random.choice(lst_var)
+        var_two = random.choice(lst_var)
+        exr = str(var_one) + ' * ' + str(var_two)
+
+        if is_not_duplicate(exr, lst_exr):
                 lst_exr[ind] = exr
-                break
+                ind += 1
 
     return lst_exr
 
 def is_not_duplicate(exr, lst_exr):
-    lst_vars = exr.split(' * ')
-    test_one = str(lst_vars[0]) + ' * ' + str(lst_vars[1])
-    test_two = str(lst_vars[1]) + ' * ' + str(lst_vars[0])
-
-    if not test_one in lst_exr:
-        return True
-
-    elif not test_two in lst_exr:
-        return True
-
-    else:
-        return False
+    return (exr not in lst_exr) and (exr[::-1] not in lst_exr)
 
 
 student_count = 15
