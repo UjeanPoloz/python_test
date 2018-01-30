b9c27596976d024478afddf7e610ee3089839172
Ievgenpolozsc <ipoloz@snapchat.com>
2018-01-30 12:12:36 +0200

    Test 8. without loop

--- test_8.py
+++ test_8.py
@@ -14,13 +14,16 @@ max_var, min_var = get_max_min_digit(list)
 
 print('\nМаксимальное число: %d\nМинимальное число: %d' % (max_var, min_var))
 
-for i, var_first in enumerate(list):
-    if var_first == max_var:
-        for ii, var_second in enumerate(list):
-            if var_second == min_var:
-                list[i] = min_var
-                list[ii] = max_var
-                break
-        break
+# for i, var_first in enumerate(list):
+#     if var_first == max_var:
+#         for ii, var_second in enumerate(list):
+#             if var_second == min_var:
+#                 list[i] = min_var
+#                 list[ii] = max_var
+#                 break
+#         break
+
+list[list.index(max_var)] = min_var
+list[list.index(min_var)] = max_var
 
 print('\nМодифицированный список: \n\t', list)
